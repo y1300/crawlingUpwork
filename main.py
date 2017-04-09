3b8ac569c9668870da8ec2496ad957fb5a99fe1e
y1300 <y1300@qq.com>
2017-04-09 18:54:47 +0100

    rm main.py

--- main.py
+++
@@ -1,4 +0,0 @@
-from scrapy import cmdline
-# cmdline.execute("scrapy crawl login".split())
-# cmdline.execute("scrapy crawl testIndependent".split())
-cmdline.execute("scrapy crawl Independent".split())
